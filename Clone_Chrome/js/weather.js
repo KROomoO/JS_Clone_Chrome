994126ee284913d94bf180dad0b6b57e10ce23d0
KROomoO <parksungik00@naver.com>
2022-06-30 21:49:50 +0900

    1.0.0

---
+++ Clone_Chrome/js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "0ba844d1366c76419b23510e9f6b38b8";
+
+
+function onGeoSuccess(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>  {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. We can't provide this service")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
